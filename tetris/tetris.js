ac602f0ee97f1b448e583b821e95977de87b5c92
Jack Nehme <jack.nehme@gmail.com>
2018-03-22 21:32:13 -0700

    completed tetris for one player

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -3,11 +3,22 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-const matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0]
-];
+function arenaSweep() {
+    let rowCount = 1;
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+
+        player.score += rowCount * 10;
+        rowCount *= 2;
+    }
+}
 
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
@@ -114,7 +125,8 @@ function playerDrop() {
         player.pos.y--;
         merge(arena, player);
         playerReset();
-        player.pos.y = 0;
+        arenaSweep();
+        updateScore();
     }
     dropCounter = 0;
 }
@@ -127,6 +139,8 @@ function playerReset() {
 
     if (collide(arena, player)) {
         arena.forEach(row => row.fill(0));
+        player.score = 0;
+        updateScore();
     }
 }
 
@@ -183,6 +197,10 @@ function update(time = 0) {
     requestAnimationFrame(update);
 }
 
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
+
 const colors = [
     null,
     '#FF0D72',
@@ -197,11 +215,9 @@ const colors = [
 const arena = createMatrix(12, 20);
 
 const player = {
-    pos: {
-        x: 5,
-        y: 5
-    },
-    matrix: createPiece('T')
+    pos: { x: 0, y: 0 },
+    matrix: null,
+    score: 0,
 }
 
 document.addEventListener('keydown', event => {
@@ -218,4 +234,6 @@ document.addEventListener('keydown', event => {
     }
 });
 
+playerReset();
+updateScore();
 update();
